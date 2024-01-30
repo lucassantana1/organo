fc6f6d1799d859477007b246e4bb1968a116f1db
lucas-santana <lucasdeoliveirasantana874@gmail.com>
2024-01-30 15:31:53 -0300

    escutando eventos

--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -14,13 +14,30 @@ const Formulario = () => {
     "Inovação e Gestão",
   ];
 
+  const aoSalvar = (evento) => {
+    evento.preventDefault();
+    console.log("Form foi submetido");
+  };
+
   return (
     <section className="formulario">
-      <form>
+      <form onSubmit={aoSalvar}>
         <h2>Preencha os dados para criar o card do colaborador</h2>
-        <CampoTexto label="Nome" placeholder="Digite seu nome" />
-        <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
-        <CampoTexto label="Imagem" placeholder="Informe o endereço da imagem" />
+        <CampoTexto
+          label="Nome"
+          obrigatorio={true}
+          placeholder="Digite seu nome"
+        />
+        <CampoTexto
+          label="Cargo"
+          obrigatorio={true}
+          placeholder="Digite seu cargo"
+        />
+        <CampoTexto
+          label="Imagem"
+          obrigatorio={false}
+          placeholder="Informe o endereço da imagem"
+        />
         <ListaSuspensa label="Time" itens={times} />
         <Botao>Criar card</Botao>
       </form>
